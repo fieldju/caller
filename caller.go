597e7e8ab613ed00ce29c4197e4908483beb41aa
Justin Field <fieldju@gmail.com>
2020-11-03 23:17:09 -0800

    fix: err logging

--- caller.go
+++ caller.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"os"
 )
 
 func main() {
-	svc := sts.New(session.New())
+	sess, err := session.NewSession(&aws.Config{
+		CredentialsChainVerboseErrors: aws.Bool(true),
+	})
+
+	if err != nil {
+		fmt.Println("failed to find valid aws credentials")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	svc := sts.New(sess)
 	input := &sts.GetCallerIdentityInput{}
 	result, err := svc.GetCallerIdentity(input)
 	if err != nil {
-		fmt.Errorf("failed to get an identity response from AWS")
-		fmt.Errorf(err.Error())
+		fmt.Println("failed to get an identity response from AWS")
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	arn := *result.Arn
-	fmt.Println(arn)
+
+	json, err := json.MarshalIndent(result, "", "\t")
+	if err != nil {
+		fmt.Println("failed to marshal aws sts response into json")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(string(json))
 }
